967302dd2cb08943be3be26a616df83252680d7c
daiki-kaneda <daiki0622developing@gmail.com>
2025-11-15 01:34:18 +0900

    Initial commit

---
+++ src/main/java/com/example/team_sudoku_api/TeamSudokuApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.team_sudoku_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamSudokuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamSudokuApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/team_sudoku_api/TeamSudokuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team_sudoku_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamSudokuApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
