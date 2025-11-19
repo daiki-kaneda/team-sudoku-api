1da7ace6ab420f09e96ccd9852422b9b4f747cbf
daiki-kaneda <daiki0622developing@gmail.com>
2025-11-20 01:12:37 +0900

    Modify AuthService to add loadOrCreateUser method

--- src/main/java/com/example/team_sudoku_api/services/AuthService.java
+++ src/main/java/com/example/team_sudoku_api/services/AuthService.java
@@ -1,8 +1,14 @@
 package com.example.team_sudoku_api.services;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Service;
 
+import com.example.team_sudoku_api.drivers.FirebaseAuthDriver;
+import com.example.team_sudoku_api.entities.User;
 import com.example.team_sudoku_api.repositories.UserRepository;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
 
 @Service
 public class AuthService {
@@ -10,5 +16,19 @@ public class AuthService {
 
     public AuthService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public User loadOrCreateUser(String idToken) throws FirebaseAuthException {
+        FirebaseToken token = FirebaseAuthDriver.verifyToken(idToken);
+        return userRepository.findById(token.getUid())
+                .orElseGet(() -> {
+                    User newUser = new User();
+                    newUser.setUid(token.getUid());
+                    newUser.setName(token.getName());
+                    newUser.setEmail(token.getEmail());
+                    newUser.setCreatedAt(LocalDateTime.now());
+                    return newUser;
+                });
+
     }
 }
