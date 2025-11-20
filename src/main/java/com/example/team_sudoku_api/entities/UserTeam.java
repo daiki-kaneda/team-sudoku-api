ef32a1df07d02fe2ea4784be22ffe29dc0a083c6
daiki-kaneda <daiki0622developing@gmail.com>
2025-11-20 23:22:25 +0900

    Fix UserTeam to supress compile error

--- src/main/java/com/example/team_sudoku_api/entities/UserTeam.java
+++ src/main/java/com/example/team_sudoku_api/entities/UserTeam.java
@@ -11,7 +11,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
