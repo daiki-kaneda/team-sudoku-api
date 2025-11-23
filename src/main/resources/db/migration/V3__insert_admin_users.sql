e95df8bbec23550ddbeabac42190778e501229df
daiki-kaneda <daiki0622developing@gmail.com>
2025-11-24 01:49:58 +0900

    Add V3__insert_admin_users.sql to insert admin user

---
+++ src/main/resources/db/migration/V3__insert_admin_users.sql
@@ -0,0 +1,20 @@
+INSERT INTO users (uid, name, email, created_at, is_active)
+VALUES (
+    '${admin.uid}',
+    '${admin.name}',
+    '${admin.email}',
+    CURRENT_TIMESTAMP(),
+    TRUE
+);
+
+INSERT INTO user_role (user_id, role_id)
+VALUES (
+    '${admin.uid}',
+    (SELECT id FROM role WHERE role_name = 'ROLE_ADMIN')
+);
+
+INSERT INTO user_role (user_id, role_id)
+VALUES (
+    '${admin.uid}',
+    (SELECT id FROM role WHERE role_name = 'ROLE_USER')
+);
