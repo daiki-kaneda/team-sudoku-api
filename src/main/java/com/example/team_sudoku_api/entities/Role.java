dcb11961538b103a791cb5a424f429ace63bd50b
daiki-kaneda <daiki0622developing@gmail.com>
2025-11-24 01:10:24 +0900

    Modify Role entity to make roleName unique

--- src/main/java/com/example/team_sudoku_api/entities/Role.java
+++ src/main/java/com/example/team_sudoku_api/entities/Role.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,6 +19,7 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String roleName;
 
     @OneToMany(mappedBy = "role", cascade = CascadeType.ALL)
