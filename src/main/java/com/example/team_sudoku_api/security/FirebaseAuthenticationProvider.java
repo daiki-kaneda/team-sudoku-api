4e6dae96e85f68db9b871d2f4eb2e13cf22f1fa2
daiki-kaneda <daiki0622developing@gmail.com>
2025-11-23 23:32:03 +0900

    Modify FirebaseAuthenticationProvider to get authorities from UserRole table

--- src/main/java/com/example/team_sudoku_api/security/FirebaseAuthenticationProvider.java
+++ src/main/java/com/example/team_sudoku_api/security/FirebaseAuthenticationProvider.java
@@ -1,26 +1,38 @@
 package com.example.team_sudoku_api.security;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import com.example.team_sudoku_api.drivers.FirebaseAuthDriver;
+import com.example.team_sudoku_api.entities.Role;
+import com.example.team_sudoku_api.services.UserRoleService;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseToken;
 
 @Component
 public class FirebaseAuthenticationProvider {
 
+    private UserRoleService userRoleService;
+
+    public FirebaseAuthenticationProvider(UserRoleService userRoleService) {
+        this.userRoleService = userRoleService;
+    }
+
     public Authentication createAuthentication(String idToken) throws FirebaseAuthException {
         FirebaseToken token = FirebaseAuthDriver.verifyToken(idToken);
         String uid = token.getUid();
-        String role = "USER";
+        List<? extends GrantedAuthority> authorities = userRoleService.getRolesByUid(uid)
+                .stream()
+                .map((r) -> new SimpleGrantedAuthority(r.getRoleName()))
+                .toList();
 
-        return new UsernamePasswordAuthenticationToken(uid, null, List.of(
-                new SimpleGrantedAuthority("ROLE_" + role)));
+        return new UsernamePasswordAuthenticationToken(uid, null, authorities);
     }
 
 }
