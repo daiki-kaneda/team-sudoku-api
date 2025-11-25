a319c322cc8d5ef264ecf7cfd2174816b5fcca8c
daiki-kaneda <daiki0622developing@gmail.com>
2025-11-26 03:08:56 +0900

    Refactor Log to use @JoinColums for composite foreign key

--- src/main/java/com/example/team_sudoku_api/entities/Log.java
+++ src/main/java/com/example/team_sudoku_api/entities/Log.java
@@ -3,11 +3,10 @@ package com.example.team_sudoku_api.entities;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import com.example.team_sudoku_api.entities.Cell.CellId;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinColumns;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
@@ -27,21 +26,25 @@ public class Log extends BaseEntity<String> {
     private String result; // success,failure
 
     @ManyToOne
-    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
-    @JoinColumn(name = "team_id", referencedColumnName = "team_id")
+    @JoinColumns({
+            @JoinColumn(name = "user_id", referencedColumnName = "user_id"),
+            @JoinColumn(name = "team_id", referencedColumnName = "team_id")
+    })
     private UserTeam userTeam;
 
     @ManyToOne
-    @JoinColumn(name = "cell_board_id", referencedColumnName = "board_id")
-    @JoinColumn(name = "cell_row", referencedColumnName = "row_idx")
-    @JoinColumn(name = "cell_column", referencedColumnName = "col_idx")
+    @JoinColumns({
+            @JoinColumn(name = "cell_board_id", referencedColumnName = "board_id"),
+            @JoinColumn(name = "cell_row", referencedColumnName = "row_idx"),
+            @JoinColumn(name = "cell_column", referencedColumnName = "col_idx")
+    })
     private Cell cell;
 
     public static Log create(boolean isCorrect, UserTeam userTeam, Cell cell) {
         Log log = new Log();
         log.id = UUID.randomUUID().toString();
         log.createdAt = LocalDateTime.now();
-        log.result = isCorrect ? "success":"failure";
+        log.result = isCorrect ? "success" : "failure";
         log.userTeam = userTeam;
         log.cell = cell;
         return log;
