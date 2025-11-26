a9f3acf2564226ac4455b1f10046258e0ad6a005
daiki-kaneda <daiki0622developing@gmail.com>
2025-11-27 03:14:24 +0900

    Add TeamQueryRepository,TeamController

---
+++ src/main/java/com/example/team_sudoku_api/repositories/TeamQueryRepository.java
@@ -0,0 +1,19 @@
+package com.example.team_sudoku_api.repositories;
+
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
+
+import com.example.team_sudoku_api.entities.Team;
+
+public interface TeamQueryRepository extends Repository<Team, String> {
+
+    @Query("SELECT t FROM Team t JOIN t.userTeams ut WHERE ut.user.uid = :userId")
+    List<Team> findJoinedTeamsByUserId(@Param("userId") String userId);
+
+    List<Team> findByNameContaining(String name, Pageable pageable);
+
+}
